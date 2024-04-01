c9fd5e76501501660272616966df631c03f3a1b0
otischung <my072814638@gmail.com>
2024-04-01 17:21:53 +0800

    [FEAT] Add senior's calculation method

--- pose.py
+++ pose.py
@@ -237,6 +237,7 @@ class PoseDetection:
     run(self):
         This function provides an infinity loop to run the main function and thus calculates angles continuously.
     """
+
     def __init__(self):
         self.video_name = "result.mp4"
         self.txt_name = 'landmarks.txt'
@@ -316,53 +317,241 @@ class PoseDetection:
         out: list
             The trajectory angles.
         """
-        # The meaning of the index is written in the doc string of the function
-        # ``get_landmark_loc``
+        # # The meaning of the index is written in the doc string of the function
+        # # ``get_landmark_loc``
+        # # 左右肩
+        # shoulder = vector_cal_idx(joint_list, 11, 12)
+        # # 肩膀 -> 手肘 (上臂)
+        # larm = vector_cal_idx(joint_list, 11, 13)
+        # # 手肘 -> 手腕 (前臂)
+        # lforearm = vector_cal_idx(joint_list, 13, 15)
+        # # 食指
+        # lindex = vector_cal_idx(joint_list, 15, 19)
+        # # 小指
+        # lpinky = vector_cal_idx(joint_list, 15, 17)
+        # # 手肘->食指
+        # lelbow_index = vector_cal_idx(joint_list, 13, 19)
+        # # 手肘->拇指
+        # lelbow_thumb = vector_cal_idx(joint_list, 13, 21)
+        # # 肩膀 -> 手肘 (上臂)
+        # rarm = vector_cal_idx(joint_list, 12, 14)
+        # # 手肘 -> 手腕 (前臂)
+        # rforearm = vector_cal_idx(joint_list, 14, 16)
+        # # 食指
+        # rindex = vector_cal_idx(joint_list, 16, 20)
+        # # 小指
+        # rpinky = vector_cal_idx(joint_list, 16, 18)
+        # # 手肘->食指
+        # relbow_index = vector_cal_idx(joint_list, 14, 20)
+        # # 手肘->拇指
+        # relbow_thumb = vector_cal_idx(joint_list, 14, 22)
+        # # 左肩到左臀
+        # lshoulder2hip = vector_cal_idx(joint_list, 11, 23)
+        #
+        # ##### J1 #####
+        # # 身體的法向量
+        # lbody_norm = np.cross(shoulder, lshoulder2hip)
+        # # print(lbody_norm)
+        #
+        # # 身體中軸
+        # shoulder_center = (get_landmark_loc(joint_list, 11) + get_landmark_loc(joint_list, 12)) / 2
+        # hip_center = (get_landmark_loc(joint_list, 23) + get_landmark_loc(joint_list, 24)) / 2
+        # body_central = hip_center - shoulder_center
+        # # print(body_central)
+        #
+        # # 身體中軸的法向量
+        # body_norm_norm = np.cross(lbody_norm, body_central)
+        # # print(body_norm_norm)
+        #
+        # return [1.57 / 2, 1.57 / 2, 1.57 / 2, 1.57 / 2, 1.57 / 2]
+
+        angle1, a1_last, a1_f = 0, 0, 0
+        angle2, a2_last, a2_f = 0, 0, 0
+        angle3, a3_last, a3_f = 0, 0, 0
+        angle4, a4_last, a4_f = 0, 0, 0
+        angle5, a5_last, a5_f = 0, 0, 0
+        angle6, a6_last, a6_f = 0, 0, 0
+
+        send_1 = 0
+        send_2 = 0
+        send_3 = 0
+        send_4 = 0
+        send_5 = 0
+
+        # 11: lshoulder / 12: rshoulder / 13: elbow / 15: wrist / 21: thumb / 17: pinky / 19: index / 23: hip
+        # 肩膀->手肘
+        arm = (
+            joint_list[13][0] - joint_list[12][0],
+            joint_list[13][1] - joint_list[12][1],
+            joint_list[13][2] - joint_list[12][2]
+        )
+        # 手肘->手腕
+        forearm = (
+            joint_list[15][0] - joint_list[13][0],
+            joint_list[15][1] - joint_list[13][1],
+            joint_list[15][2] - joint_list[13][2]
+        )
         # 左右肩
-        shoulder = vector_cal_idx(joint_list, 11, 12)
-        # 肩膀 -> 手肘 (上臂)
-        larm = vector_cal_idx(joint_list, 11, 13)
-        # 手肘 -> 手腕 (前臂)
-        lforearm = vector_cal_idx(joint_list, 13, 15)
+        shoulder = (
+            joint_list[11][0] - joint_list[12][0],
+            joint_list[11][1] - joint_list[12][1],
+            joint_list[11][2] - joint_list[12][2]
+        )
         # 食指
-        lindex = vector_cal_idx(joint_list, 15, 19)
+        index = (
+            joint_list[19][0] - joint_list[15][0],
+            joint_list[19][1] - joint_list[15][1],
+            joint_list[19][2] - joint_list[15][2]
+        )
         # 小指
-        lpinky = vector_cal_idx(joint_list, 15, 17)
+        pinky = (
+            joint_list[17][0] - joint_list[15][0],
+            joint_list[17][1] - joint_list[15][1],
+            joint_list[17][2] - joint_list[15][2]
+        )
         # 手肘->食指
-        lelbow_index = vector_cal_idx(joint_list, 13, 19)
+        elbow_index = (
+            joint_list[19][0] - joint_list[13][0],
+            joint_list[19][1] - joint_list[13][1],
+            joint_list[19][2] - joint_list[13][2]
+        )
         # 手肘->拇指
-        lelbow_thumb = vector_cal_idx(joint_list, 13, 21)
-        # 肩膀 -> 手肘 (上臂)
-        rarm = vector_cal_idx(joint_list, 12, 14)
-        # 手肘 -> 手腕 (前臂)
-        rforearm = vector_cal_idx(joint_list, 14, 16)
-        # 食指
-        rindex = vector_cal_idx(joint_list, 16, 20)
-        # 小指
-        rpinky = vector_cal_idx(joint_list, 16, 18)
-        # 手肘->食指
-        relbow_index = vector_cal_idx(joint_list, 14, 20)
-        # 手肘->拇指
-        relbow_thumb = vector_cal_idx(joint_list, 14, 22)
-        # 左肩到左臀
-        lshoulder2hip = vector_cal_idx(joint_list, 11, 23)
+        elbow_thumb = (
+            joint_list[21][0] - joint_list[13][0],
+            joint_list[21][1] - joint_list[13][1],
+            joint_list[21][2] - joint_list[13][2]
+        )
+        # 肩膀->骨盆
+        hip_shou = (
+            joint_list[12][0] - joint_list[23][0],
+            joint_list[12][1] - joint_list[23][1],
+            joint_list[12][2] - joint_list[23][2]
+        )
 
-        ##### J1 #####
-        # 身體的法向量
-        lbody_norm = np.cross(shoulder, lshoulder2hip)
-        # print(lbody_norm)
+        #### calculate angle
+        if joint_list[12][3] > 0.8 and joint_list[13][3] > 0.8 and joint_list[15][3] > 0.8:
+            index_pinky = np.cross(index, pinky)
+            arm_fore = np.cross((-arm[0], -arm[1], -arm[2]), forearm)
 
-        # 身體中軸
-        shoulder_center = (get_landmark_loc(joint_list, 11) + get_landmark_loc(joint_list, 12)) / 2
-        hip_center = (get_landmark_loc(joint_list, 23) + get_landmark_loc(joint_list, 24)) / 2
-        body_central = hip_center - shoulder_center
-        # print(body_central)
+            # J1角度
+            J1 = round(math.degrees(angle((arm[0], 0, arm[2]), (1, 0, 0))), 3)
+            # J1方向
+            dir_a1 = dotproduct((0, 0, arm[2]), (0, 0, 1))
+            if dir_a1 != 0:
+                dir_a1 /= abs(dir_a1)
+                angle1 = J1 * dir_a1
+            else:
+                angle1 = J1 * dir_a1
+            # J1: -165~+165
+            if angle1 > 163:
+                angle1 = 163
+            elif angle1 < -163:
+                angle1 = -163
+            else:
+                pass
+            if abs(abs(angle1) - abs(a1_last)) <= 1:
+                pass
+            else:
+                a1_f = angle1
+                send_1 += 1
+            a1_last = angle1
 
-        # 身體中軸的法向量
-        body_norm_norm = np.cross(lbody_norm, body_central)
-        # print(body_norm_norm)
+            # J2角度: -125 ~ +85->70
+            J2 = round(math.degrees(angle((shoulder[0], shoulder[1], 0), (arm[0], arm[1], 0))), 1)
+            # J2方向
+            if joint_list[13][1] > joint_list[12][1]:
+                angle2 = -J2
+            elif joint_list[13][1] <= joint_list[12][1]:
+                angle2 = J2
 
-        return [1.0, 1.0, 1.0, 1.0, 1.0]
+            if angle2 > 70:
+                angle2 = 70
+            elif angle2 < -60:
+                angle2 = -60
+            else:
+                pass
+            if abs(abs(angle2) - abs(a2_last)) <= 1:
+                send_2 = 0
+            else:
+                a2_f = angle2
+                send_2 += 1
+            a2_last = angle2
+
+            # J3角度: -55 ~ +185
+            J3 = round(math.degrees(angle((arm[0], arm[1], 0), (forearm[0], forearm[1], 0))), 1)
+            # J3方向
+            if joint_list[15][1] < joint_list[13][1]:
+                angle3 = J3 + 90
+            elif joint_list[15][1] >= joint_list[13][1]:
+                angle3 = -J3 + 90
+
+            if angle3 > 180:
+                angle3 = 180
+            elif angle3 < -45:
+                angle3 = -45
+            else:
+                pass
+            if abs(abs(angle3) - abs(a3_last)) <= 1:
+                send_3 = 0
+            else:
+                a3_f = angle3
+                send_3 += 1
+            a3_last = angle3
+
+            # J4角度: -190 ~ +190
+            J4 = round(math.degrees(angle(index_pinky, arm_fore)), 1)
+            # J4方向
+            if J4 == 0:
+                angle4 = -J4
+            elif J4 == 180:
+                angle4 = J4
+            else:
+                cross_hand_elb = np.cross(index_pinky, arm_fore)
+                dir_a4 = dotproduct(cross_hand_elb, forearm)
+                dir_a4 /= abs(dir_a4)
+                angle4 = J4 * dir_a4
+
+            if angle4 > 90:
+                angle4 = 90
+            elif angle4 < -90:
+                angle4 = -90
+            else:
+                pass
+            if abs(abs(angle4) - abs(a4_last)) <= 1:
+                pass
+            else:
+                a4_f = angle4
+                send_4 += 1
+            a4_last = angle4
+
+            # J5角度: -115 ~ +115
+            J5 = round(math.degrees(angle(forearm, index)), 1)
+            # J5方向
+            if joint_list[19][1] >= joint_list[15][1]:
+                angle5 = -J5
+            elif joint_list[19][1] < joint_list[15][1]:
+                angle5 = J5
+
+            if angle5 > 110:
+                angle5 = 110
+            elif angle5 < -110:
+                angle5 = -110
+            else:
+                pass
+            if abs(abs(angle5) - abs(a5_last)) <= 1:
+                pass
+            else:
+                a5_f = angle5
+                send_5 += 1
+            a5_last = angle5
+
+            result = [math.radians(deg) for deg in [a1_f, a2_f, a3_f, a4_f, a5_f]]
+            # Calibration for Unity
+            result = [a + b for (a, b) in
+                      zip(result, [math.radians(90), math.radians(90), 0, math.radians(90), math.radians(90)])]
+            print(f"Result: {result}")
+
+            return result
 
     def main(self) -> tuple[bool, None | list]:
         """
